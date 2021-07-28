610cbc39fa2141846b19ef1265f61adf35fe1b03
RyanDritama <77050030+RyanDritama@users.noreply.github.com>
2021-07-28 18:04:50 +0700

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,35 @@
+"""gps URL Configuration
+
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path,include
+from koordinat.views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('koordinat', KoordinatViewSet)
+
+urlpatterns = [
+    path('', include('monitor.urls')),
+    path('', include('rest.urls')),
+    path('user/', include('users.urls')),
+    path('user/', include('django.contrib.auth.urls')),
+    path('admin/', admin.site.urls),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('api/', include(router.urls)),
+]
+
